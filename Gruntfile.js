a459d5739499237c5597bf464ee320bcef3fc52c
Krzysztof Zielonka <savespace@outlook.com>
2017-09-03 15:37:52 +0200

    one_upd

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+      sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'sass/style.sass',
+          'css/grid.css': 'sass/grid.sass'
+        }
+      }
+    },
+
+    jshint: {
+      all: ['js/*.js']
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.registerTask('default', ['jshint', 'sass']);
+};
---
+++ js/script.js
@@ -0,0 +1,2 @@
+window.sr = ScrollReveal();
+sr.reveal('h2, p, img', { delay: 300 })
